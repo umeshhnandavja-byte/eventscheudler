e5b5c14a75645b03f50bb95a6e3747152973b93c
umeshhnandavja-byte <umeshhnanda.vja@gmail.com>
2026-05-17 23:12:44 +0530

    new div

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-let board = document.getElementById("board");
-let context = board.getContext("2d");
-
-let mouse_down = function(event){
-    event.preventDefault();
-    console.log(event);
-}
-
-board.onmousedown = mouse_down;
+const canvas = getElementById('board');
+const canvaswrap = getElementById('canvas-w')
 
 document.getElementById('date').innerHTML = Date();
+
+const newDiv = document.createElement('div');
+newDiv.innerHTML = "I'm a new div!";
+
+function newdiv(){
+    Object.assign(newDiv.style, {
+        position: 'absolute',
+        left:  '100px',
+        top: '100px',
+        backgroundColor: 'rgba(255, 255, 0, 0.8)',
+        padding: '5px',
+        border: '1px solid black',
+        pointerEvents: 'none'
+    });
+
+    canvaswrap.appendChild(newDiv);
+}
